a42ed568d0ac1133facd009c26b0277b279c4ca7
HimanshuKasundra <himanshukasundra153@gmail.com>
2023-02-14 22:28:29 +0530

    first commit

--- crud/lib/crud.dart
+++ crud/lib/crud.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:crud/add_user_page.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Crud extends StatefulWidget {
   const Crud({Key? key}) : super(key: key);
@@ -21,11 +24,30 @@ class _CrudState extends State<Crud> {
               Navigator.of(context)
                   .push(MaterialPageRoute(builder: (context) => AddUser()));
             },
-            child: Icon(Icons.add,color: Colors.white),
+            child: Icon(Icons.add, color: Colors.white),
           )
         ],
       ),
-          body: FutureBuilder(builder: builder),
+      body: FutureBuilder<Map<String,dynamic>>(
+        builder: (context, snapshot) {
+          if (snapshot.data != null && snapshot.hasData) {
+            return Container(
+              child: Text(snapshot.data![''][0]['id'].toString()),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+        future: callApiGet(),
+      ),
     ));
+  }
+
+  Future<Map<String,dynamic>> callApiGet() async {
+    http.Response res = await http
+        .get(Uri.parse("https://6315f77133e540a6d3894dc8.mockapi.io/student"));
+    Map<String,dynamic> map= jsonDecode(res.body.toString());
+    // print("Dat ::: ${res.body.toString()}");
+    return map;
   }
 }
